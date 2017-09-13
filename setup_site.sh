8a6635cbaabcfd90df39647d3e2bb9b3c4ffce05
Michael McEniry [UAH] <mcenirm@uah.edu>
2017-09-12 19:59:35 -0500

    Lots of convoluted steps to get drush to do something

---
+++ setup_site.sh
@@ -0,0 +1,17 @@
+export PGHOST=${PGHOST:-postgres}
+export PGPORT=${PGPORT:-5432}
+export PGDATABASE=${PGDATABASE:-postgres}
+export PGUSER=${PGUSER:-postgres}
+export PGPASSWORD=${PGPASSWORD:-postgres}
+
+wait_for_database
+
+if ! drush status bootstrap | grep -q Successful ; then
+  drush -y site-install \
+      --db-url=pgsql://"$PGUSER":"$PGPASSWORD"@"$PGHOST":"$PGPORT"/"$PGDATABASE" || rv=$?
+  if [ "$rv" -ne 0 ] ; then
+    echo drush site-install returned: "$rv"
+  fi
+fi
+
+drush uli
